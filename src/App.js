7a27af456ae20213c280ee81ecef2c157ee8bdf5
robert <rsmith8917@gmail.com>
2022-03-12 21:34:56 -0500

    css

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import './App.css';
 import { faBars, faCircleQuestion, faChartColumn, faGear } from '@fortawesome/free-solid-svg-icons';
 import IconButton from './components/IconButton';
+import GameBoard from './components/GameBoard';
 
 function App() {
   return (
-    <div className='header'>
-      <div className='header-left'>
-        <IconButton icon={faBars} />
-        <IconButton icon={faCircleQuestion} />
+    <div className='root'>
+      <div className='header'>
+        <div className='header-left'>
+          <IconButton icon={faBars} />
+          <IconButton icon={faCircleQuestion} />
+        </div>
+        <div className='header-center'>
+          <span className='title'>Wordle</span>
+        </div>
+        <div className='header-right'>
+          <IconButton icon={faChartColumn} />
+          <IconButton icon={faGear} />
+        </div>
       </div>
-      <div className='header-center'>
-        <span className='title'>Wordle</span>
+      <div className='main'>
+        <GameBoard />
       </div>
-      <div className='header-right'>
-        <IconButton icon={faChartColumn} />
-        <IconButton icon={faGear} />
+      <div className='keyboard'>
+        Keyboard
       </div>
     </div>
   );
---
+++ src/components/GameBoard.js
@@ -0,0 +1,112 @@
+import './GameBoard.css'
+
+function GameBoard() {
+    return (
+        <div className='game-board'>
+            {/* <div className='game-board-row'>
+                <div className='game-board-letter'>
+                    T
+                </div>
+                <div className='game-board-letter'>
+                    O
+                </div>
+                <div className='game-board-letter'>
+                    A
+                </div>
+                <div className='game-board-letter'>
+                    S
+                </div>
+                <div className='game-board-letter'>
+                    T
+                </div>
+            </div>
+            <div className='game-board-row'>
+                <div className='game-board-letter'>
+                    T
+                </div>
+                <div className='game-board-letter'>
+                    O
+                </div>
+                <div className='game-board-letter'>
+                    A
+                </div>
+                <div className='game-board-letter'>
+                    S
+                </div>
+                <div className='game-board-letter'>
+                    T
+                </div>
+            </div>
+            <div className='game-board-row'>
+                <div className='game-board-letter'>
+                    T
+                </div>
+                <div className='game-board-letter'>
+                    O
+                </div>
+                <div className='game-board-letter'>
+                    A
+                </div>
+                <div className='game-board-letter'>
+                    S
+                </div>
+                <div className='game-board-letter'>
+                    T
+                </div>
+            </div>
+            <div className='game-board-row'>
+                <div className='game-board-letter'>
+                    T
+                </div>
+                <div className='game-board-letter'>
+                    O
+                </div>
+                <div className='game-board-letter'>
+                    A
+                </div>
+                <div className='game-board-letter'>
+                    S
+                </div>
+                <div className='game-board-letter'>
+                    T
+                </div>
+            </div>
+            <div className='game-board-row'>
+                <div className='game-board-letter'>
+                    T
+                </div>
+                <div className='game-board-letter'>
+                    O
+                </div>
+                <div className='game-board-letter'>
+                    A
+                </div>
+                <div className='game-board-letter'>
+                    S
+                </div>
+                <div className='game-board-letter'>
+                    T
+                </div>
+            </div>
+            <div className='game-board-row'>
+                <div className='game-board-letter'>
+                    T
+                </div>
+                <div className='game-board-letter'>
+                    O
+                </div>
+                <div className='game-board-letter'>
+                    A
+                </div>
+                <div className='game-board-letter'>
+                    S
+                </div>
+                <div className='game-board-letter'>
+                    T
+                </div>
+            </div> */}
+        </div>
+    );
+}
+
+export default GameBoard;
